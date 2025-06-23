d4bd1c6eb8717c7ae3160ba413564fcd8d5de0fa
handa01kim <yi.kim@handalab.com>
2025-06-23 10:56:41 +0900

    stash

--- temp-direct8080.sh
+++ temp-direct8080.sh
@@ -1,2 +1,2 @@
-docker exec router_a iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination 172.16.200.3:80
-docker exec router_a iptables -t nat -A POSTROUTING -j MASQUERADE
+# docker exec router_a iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination 172.16.200.3:80
+# docker exec router_a iptables -t nat -A POSTROUTING -j MASQUERADE
