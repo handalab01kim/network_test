43529fdc596acb203abc18a45a3b9fac4f1be0dd
handa01kim <yi.kim@handalab.com>
2025-06-23 11:54:13 +0900

    재구성, ping 확인 아직 안됨

--- clear.sh
+++ clear.sh
@@ -1,6 +1,4 @@
 
-
 # !! 리소스 정리
 docker compose down
-docker network rm networks_link-host-a networks_link-a-b networks_net-a networks_net-b
-
+docker network rm networks_host-connection networks_link-a-b networks_net-a networks_net-b
--- test.sh
+++ test.sh
@@ -1,13 +1,5 @@
+docker exec pc_a ping -c 3 172.16.200.100
 
-######
+#docker exec pc_c ping -c 3 172.16.0.100
 
-#!! 테스트
-# A 라우터에서 pc_b 서버로 curl
-docker exec router_a wget -O- http://172.16.200.3:80
-
-# or
-# host에서 router_a를 통해 router_b → pc_b로 가는 트래픽 경유 시 확인용
-# 포트 열림 확인
-docker exec pc_b netstat -tnlp
-
-
+#docker exec pc_a traceroute 172.16.200.100
